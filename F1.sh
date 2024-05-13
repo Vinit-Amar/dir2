3c90b52f09f6588a24b5dbc24856cf36b7dc7b25
vinit Amar <vinit.amar@gmail.com>
2024-05-13 03:08:01 +0000

    Added uppercase in dev branch

--- F1.sh
+++ F1.sh
@@ -3,9 +3,9 @@ echo "Enter a character to check vowel or not "
 read char
 case $char in
     [aeiouAEIOU])
-        echo "It is a lowercase vowel"
+        echo "It is a  vowel"
         ;;
     *)
-        echo "It is not a lowercase vowel"
+        echo "It is not a vowel"
         ;;
 esac
